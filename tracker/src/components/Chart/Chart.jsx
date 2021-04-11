939714a4b21268d2b0ab68283cd4f9525489fe71
vivek22117 <vivekmishra22117@gmail.com>
2021-04-11 21:40:19 +0530

    updating chart module

--- tracker/src/components/Chart/Chart.jsx
+++ tracker/src/components/Chart/Chart.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useEffect, useState } from "react";
 import { fetchDailyData } from "../../api";
-import { Line } from "react-chartjs-2";
+import { Line, Bar } from "react-chartjs-2";
 
 import styles from './Chart.module.css';
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
 
     const [dailyData, setDailyData] = useState([]);
 
@@ -16,21 +16,21 @@ const Chart = () => {
         }
 
         fetchAPI();
-    });
+    }, []);
 
     const lineChart = (
       dailyData.length
         ? (
           <Line
             data={{
-                labels: dailyData(({ date }) => date),
+                labels: dailyData.map(({ date }) => date),
                 datasets: [{
-                    data: dailyData(({ confirmed }) => confirmed),
+                    data: dailyData.map(({ confirmed }) => confirmed),
                     label: 'Infected',
                     borderColor: '#3333ff',
                     fill: true,
                 }, {
-                    data: dailyData(({ deaths }) => deaths),
+                    data: dailyData.map(({ deaths }) => deaths),
                     label: 'Deaths',
                     borderColor: 'red',
                     backgroundColor: 'rgba(255, 0, 0, 0.5)',
@@ -41,9 +41,35 @@ const Chart = () => {
         ) : null
     );
 
+    // console.log(confirmed.value, recovered.value, deaths.value);
+
+    const barChart = (
+      confirmed
+      ? (
+          <Bar
+            data={{
+              labels: ['Infected', 'Recovered', 'Deaths'],
+              datasets: [{
+                label: 'People',
+                backgroundColor: [
+                  'rgba(0, 0, 255, 0.5)',
+                  'rgba(0, 255, 0, 0.5)',
+                  'rgba(255, 0, 0, 0.5)',
+                ],
+                data: [confirmed.value, recovered.value, deaths.value]
+              }]
+            }}
+            options={{
+              legend: {display: false},
+              title: {display: true, text:`Current state in ${country}`},
+            }}
+          />
+        ) : null
+    );
+
     return (
-      <div>
-        {lineChart}
+      <div className={styles.container}>
+        {country ? barChart : lineChart}
       </div>
     )
 }
